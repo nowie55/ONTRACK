4e4da0e4485b26d777b1e45297d16ab3f75dc3ae
nowie55 <enofestus.2010@gmail.com>
2019-10-18 21:01:07 +0100

    Create Employee Functionality Implemented Successfully

---
+++ Dependencies/js/employee.js
@@ -0,0 +1,55 @@
+var employee = [];        //an array used to store all employee         
+var container;          //define html contents for the table
+
+
+//this is used to add a new employee to the system
+$('#createEmployee').click(function () {
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var email = $("#email").val();
+    var department = $("#department").val();
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:3000/employee',
+        data: { firstName: firstName, lastName: lastName, email: email, department: department}, //, attendance: false
+        success: function (result) {
+            alert('New Employee created');
+            location.reload();
+        }
+    })
+
+})
+
+
+
+
+function load() {                                   //this loads data from a server and 
+    //puts the returned data into the selected element
+    $.getJSON("db.json", function (json) {
+        employee = json.employee;
+        container;
+        populateTable(employee);
+    });
+}
+//method for generating the body of the dynamic table
+function populateTable(data) {
+    document.getElementById('body').innerHTML = " ";
+    for (let i = 0; i < employee.length; i++) {
+        container = document.createElement("tr");
+        container.innerHTML = `
+                                               <td class="row-id">${data[i]['id']}</td>
+                                               <td>${data[i]['firstName']}</td>
+                                               <td>${data[i]['lastName']}</td>
+                                               <td>${data[i]['email']}</td>
+                                               <td>${data[i]['department']}</td>
+                                               <button class="btn btn-sm btn-primary delete">Edit</button>           
+                                               <button class="btn btn-sm btn-danger delete">Delete</button>
+                                             </td> 
+                                                </tr>`;
+        document.getElementById('body').appendChild(container);
+    }
+
+}
+//<td><input type="checkbox" class="attendance-box" ${data[i]['attendance'] == 'true' ? 'checked' : ''} /></td>
+//<td>
---
+++ Dependencies/js/login.js
@@ -0,0 +1,94 @@
+
+/*$(document).ready(function () {
+    $.get("http://localhost:3000/admin", function (data, status) {
+
+    console.log(data);
+        $('.login').click(function () {
+            let email = $("#email").val();
+            let password = $("#password").val();
+            for (i = 0; i < data.length; i++) {
+
+                window.location.assign("/employee.html");
+
+            }
+        });
+    });
+});*/
+
+
+
+
+
+
+$(document).ready(function (){
+    $.get("http://localhost:3000/admin", function (data, status) { 
+        console.log(data); 
+        $('#login').click(function(){
+            let email = $('#email').val();
+            console.log("This is my  email", email);
+            let password = $('#password').val();
+            if ((email === "" ||email.length < 0) || (password.length < 0 || password === ""))
+                {
+                    alert("Please fill all fields");
+                } 
+            else
+                {
+            console.log(password) 
+            
+                for(let i=0; i < data.length; i++)
+                    {      
+                if (password === data[i].password && email === data[i].email)
+            
+                    {
+                        window.location.assign("/employee.html");
+                    }
+                else 
+                    {
+                        alert("Invalid email/password");
+                    }        
+                    }
+                }
+        });
+    });
+
+}); 
+
+/*
+$$(document).ready({function loginUser() {
+  $('#login-form').submit(function (e) {
+    e.preventDefault();
+    const email = $('#email').val();
+    const password = $('#password').val();
+    const url = $(this).attr('action');
+    if (email.length < 1) {
+      alert("Please enter your email address");
+    } else if (password.length < 1) {
+      alert("Please enter your password");
+    } else {
+      $.ajax({
+        url: url + `/?email=${email}&&password=${password}`,
+        type: 'GET',
+        dataType: 'json'
+      }).done((data) => {
+        // Save to localStorage
+        localStorage.setItem('user', JSON.stringify(data[0]));
+        // Retrieve from localStorage
+        const user = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user'));
+        console.log(data)
+        if (data.length === 0) {
+          alert('User login credentials incorrect')
+        } else {
+          if (data.isAdmin !== true) {
+            window.location.replace('../html/create.html')
+          } else {
+            window.location.replace('../html/admin.html')
+          }
+        }
+      })
+    }
+    return false;
+  })
+});
+
+
+*/
